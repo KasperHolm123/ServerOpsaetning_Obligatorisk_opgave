7abae7d589b9a52653a975c02c2738d847961449
Kasper Holm <kasper.marhauer@gmail.com>
2022-03-09 12:43:21 +0100

    Connect til server test

--- ServerOpsaetning/View/ServerDetailsView.xaml.cs
+++ ServerOpsaetning/View/ServerDetailsView.xaml.cs
@@ -1,5 +1,4 @@
-﻿using ServerOpsaetning.ViewModel;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,20 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ServerOpsaetning
+namespace ServerOpsaetning.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for ServerDetailsView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ServerDetailsView : Window
     {
-        MainViewModel model = new MainViewModel();
-        public MainWindow()
+        public ServerDetailsView()
         {
-            DataContext = model;
             InitializeComponent();
         }
     }
---
+++ ServerOpsaetning/ViewModel/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Renci.SshNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerOpsaetning.ViewModel
+{
+    class MainViewModel
+    {
+        
+        public MainViewModel()
+        {
+            ConnectToServer();
+        }
+
+        private void ConnectToServer()
+        {
+            var connectionInfo = new ConnectionInfo("192.168.9.179", "kasper",
+                                                    new PasswordAuthenticationMethod("kasper", "kasper123"));
+            using (var client = new SshClient("192.168.1.179", "kasper", "kasper123"))
+            {
+                client.Connect();
+            }
+        }
+    }
+}
